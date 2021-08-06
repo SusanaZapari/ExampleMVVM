2b53d811822d672d0f32a6bfb7cce0443f049782
SusanaZapari <zapari.developer@gmail.com>
2021-08-06 11:35:30 -0500

    Initial commit

---
+++ ExampleMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExampleMVVM
+//
+//  Created by susy zapari on 8/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
